1779ac2feec4567db3388076d1f4fc5546940f0e
Warren Joubert <wjoubert@microsoft.com>
2022-09-22 16:45:47 +0200

    updated terraform file

--- main.tf
+++ main.tf
@@ -1,8 +1,36 @@
+
+
+variables:
+  - group: DEV
+
+variable "project_name" {
+    default = "Sample Terraform Project"
+}
+
+variable "description" {
+    default = "Sample Azure DevOps Project Created Using Terraform"
+}
+
+variable "visibility" {
+    default = "private"
+    #Options private, public
+}
+
+variable "version_control" {
+    default = "Git"
+    #Options Git, Tfvc
+}
+
+variable "work_item_template" {
+    default = "Agile"
+    #Options Agile, Basic, CMMI, Scrum
+}
+
 provider "azuredevops" {
   version = ">= 0.0.1"
   # Remember to specify the org service url and personal access token details below
   org_service_url = "https://dev.azure.com/wjoubert0382"
-  personal_access_token = "xxxxxxx"
+  personal_access_token = $(PAT)
 }
 
 resource "azuredevops_project" "terraform_ado_project" {
